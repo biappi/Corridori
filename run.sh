3621a44dc899662e9de4666a9b5d86d2a15fb3ee
Antonio Willy Malara <antonio.malara@gmail.com>
2021-10-05 12:26:33 +0200

    dont use manager overlay after all, there is not much dos memory

--- run.sh
+++ run.sh
@@ -5,8 +5,7 @@
     -c "set path=c:/tc;c:/tasm" \
     -c "copy tr\\cods\\arcade.exe tr\\cods\\arca.exe" \
     -c "copy arca.exe tr\\cods\\arcade.exe" \
-    -c "copy mana.exe tr" \
     -c "cd tr" \
-    -c "mana"
+    -c "manager"
 
 
