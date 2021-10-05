b0049e917e04c18a69e3d90b261eb56186cecb7f
Antonio Willy Malara <antonio.malara@gmail.com>
2021-10-05 12:26:33 +0200

    now we can overlay both arcade and manager

--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@
     -c "mount c build" \
     -c "c:"\
     -c "set path=c:/tc;c:/tasm" \
+    -c "copy tr\\cods\\arcade.exe tr\\cods\\arca.exe" \
+    -c "copy arca.exe tr\\cods\\arcade.exe" \
     -c "copy mana.exe tr" \
     -c "cd tr" \
     -c "mana"
