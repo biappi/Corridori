2eb0635719521e1d5147863c64060bb7ab50005d
Antonio Willy Malara <antonio.malara@gmail.com>
2023-09-22 18:22:07 +0200

    github action to build wasm

---
+++ wasm/actual-build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+cd src &&                                       \
+  emcc                                          \
+    -O3                                         \
+    corridori.c                                 \
+    -o index.html                               \
+    -I../wasm/raylib-4.2.0_webassembly/include/ \
+    -L../wasm/raylib-3.7.0_webassembly/lib      \
+    -I../wasm/cimgui/                           \
+    ../wasm/cimgui/cimgui.a                     \
+    -s USE_GLFW=3                               \
+    --preload-file ../dos/tr@/                  \
+    -lraylib -s ASYNCIFY
+
--- wasm/build.sh
+++ wasm/build.sh
@@ -1,6 +1,7 @@
-docker run \
-    -it \
-    --rm \
-    -v "$PWD/..":/src emscripten/emsdk \
-    /usr/bin/bash -c "cd src && emcc -O3 corridori.c -o index.html -I../wasm/raylib-4.2.0_webassembly/include/ -L../wasm/raylib-3.7.0_webassembly/lib  -I../wasm/cimgui/ ../wasm/cimgui/cimgui.a -s USE_GLFW=3 --preload-file ../dos/tr@/ -lraylib -s ASYNCIFY"
+docker run               \
+    -it                  \
+    --rm                 \
+    -v "$PWD/..":/src    \
+    emscripten/emsdk     \
+    wasm/actual-build.sh
 
